eeeddeb67546dd1f33d6e68a2e3d04af385753c1
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-04-11 14:09:40 +0600

    navbar created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,112 @@
-import React from 'react';
+import React from 'react'; 
+import { useState } from 'react';
+import { BriefcaseIcon,Bars3BottomRightIcon,BoltIcon,XMarkIcon } from '@heroicons/react/24/solid'
+
+import { Link,NavLink } from 'react-router-dom';
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
-        <div>
-            <h1>Header page</h1>
+        <div className='bg-gray-100 flex items-center justify-between'>
+
+            {/* logo */}
+<div className='inline-flex items-center'>
+    <BriefcaseIcon className='h-8 w-8 text-yellow-300'></BriefcaseIcon>
+    <h1 className='font-bold text-4xl ml-2 tracking-wide'>Job Coach</h1>
+    </div>
+            {/* nav item */}
+
+            <ul className='items-center hidden space-x-8 lg:flex'>
+          <li>
+            <NavLink
+              to='/'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Statistics
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/AppliedJobs'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Applied Jobs
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/Blogs'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Blogs
+            </NavLink>
+          </li>
+        </ul>
+        {/* Mobile Navbar Section */}
+        <div className='lg:hidden'>
+          {/* Dropdown Open Button */}
+          <button
+            aria-label='Open Menu'
+            title='Open Menu'
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <Bars3BottomRightIcon className='w-5 text-gray-600' />
+          </button>
+          {isMenuOpen && (
+            <div className='absolute top-0 left-0 w-full z-10'>
+              <div className='p-5 bg-white border rounded shadow-sm'>
+                {/* Logo & Button section */}
+                <div className='flex items-center justify-between mb-4'>
+                  <div>
+                    <Link to='/' className='inline-flex items-center'>
+                      <BoltIcon className='h-6 w-6 text-blue-500' />
+                      <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+                        Job Coach
+                      </span>
+                    </Link>
+                  </div>
+                  {/* Dropdown menu close button */}
+                  <div>
+                    <button
+                      aria-label='Close Menu'
+                      title='Close Menu'
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <XMarkIcon className='w-5 text-gray-600' />
+                    </button>
+                  </div>
+                </div>
+                {/* Mobile Nav Items Section */}
+                <nav>
+                  <ul className='space-y-4'>
+                    <li>
+                      <Link to='/' className='default'>
+                        Statistics
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/AppliedJobs'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        AppliedJobs
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/Blogs'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        Blogs
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
         </div>
+      </div>   
     );
 };
 
