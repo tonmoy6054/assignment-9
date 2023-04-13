9981643036602d1d8535171daf77cc269053cd48
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-04-14 02:36:15 +0600

    sectionOne created

--- src/components/Cards/JobCard.jsx
+++ src/components/Cards/JobCard.jsx
@@ -11,9 +11,16 @@ const JobCard = () => {
 
 
   return (
-    <div>
-      
+    <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 m-5'>
+{catagories.map(catagory => (
+       <li key={catagory.id}>
+            <img className='w-10 h-9' src="{catagory.logo}"></img>
+            <h2 className='font-bold text-2xl'>{catagory.title}</h2>
+            <p>{catagory.jobs}</p>
+            </li>
+))}
     </div>
+
   );
 };
 
